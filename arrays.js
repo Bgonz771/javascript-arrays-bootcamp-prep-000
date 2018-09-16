da4a2728b9cb31f9c16e74fb92bc9a1632e0c6cd
billy <billy@arkhamgc.com>
2018-09-16 20:54:49 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,40 @@
+var chocolateBars = 
+[ "snickers", "hundered grand", "kitkat", "skittles" ];
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element);
+  return array;
+}
+
+ function addElementToBeginningOfArray(array, element){
+   array = [element,... array];
+   return array;
+   
+ }
+
+ function destructivelyAddElementToEndOfArray (array, element){
+   array.push(element);
+   return array;
+ } 
+ 
+ function addElementToEndOfArray(array, element){
+   array = [... array, element];
+   return array;
+ } 
+ 
+ function accessElementInArray(array, index){
+   return (array[index]);
+   
+ }
+ 
+ function destructivelyRemoveElementFromBeginningOfArray(array){
+   return array.shift();
+
+}
+
+function removeElementFromBeginningOfArray(array){
+   return array.slice(1);
+   
+}   
+   
+   
